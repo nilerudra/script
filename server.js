6e74ddaed24c2e00ef5326c2f75787249c3c1f8c
Rudra <105378849+nilerudra@users.noreply.github.com>
2026-03-23 17:55:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get("/script.js", (req, res) => {
   res.sendFile(__dirname + "/public/script.js");
 });
 
+app.get("/index.html", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/checkout.html", (req, res) => {
+  res.sendFile(__dirname + "/checkout.html");
+});
+
 app.listen(3000, () => {
   console.log("Server running");
 });
