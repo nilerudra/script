dd937750b3b3da963b7e166d85ca60527575745f
Rudra <105378849+nilerudra@users.noreply.github.com>
2026-04-07 16:05:08 +0530

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -132,6 +132,16 @@
     }
   }
 
+  async function generateHash(message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+    return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+  }
+
   // sends cart data to backend server, it will return the Checkout URL
   async function createCheckoutSession(cart) {
     const payload = {
